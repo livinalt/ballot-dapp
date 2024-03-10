ec6ec5b1ad15603e86ded8b308dedbcc64ec130c
Jeremiah Samuel <livinalt@gmail.com>
2024-03-10 04:55:23 +0100

    update Handle

--- src/App.jsx
+++ src/App.jsx
@@ -5,51 +5,17 @@ import Header from "./Components/Header";
 import Proposal from "./Components/Proposal";
 import DelegateVote from "./Components/DelegateVote";
 import useProposals from "./hooks/useProposals";
-import { useWeb3ModalAccount,useWeb3ModalProvider} from "@web3modal/ethers/react";
-import { isSupportedChain } from "./Utils";
-import { getProvider } from "./Constants/providers";
-import { getProposalsContract } from "./Constants/contracts";
+// import { useWeb3ModalAccount,useWeb3ModalProvider} from "@web3modal/ethers/react";
+// import { isSupportedChain } from "./Utils";
+// import { getProvider } from "./Constants/providers";
+// import { getProposalsContract } from "./Constants/contracts";
+import HandleVoter from "./Components/HandleVoter";
+
 
 configureWeb3Modal();
 
 function App() {
     const { loading, data: proposals } = useProposals();
-    const { chainId } = useWeb3ModalAccount();
-    const { walletProvider } = useWeb3ModalProvider();
-
-    const handleVote = async (id) => {
-        if (!isSupportedChain(chainId)) return console.error("Wrong network");
-        const readWriteProvider = getProvider(walletProvider);
-        const signer = await readWriteProvider.getSigner();
-
-        const contract = getProposalsContract(signer);
-
-        try {
-            const transaction = await contract.vote(id);
-            console.log("transaction: ", transaction);
-            const receipt = await transaction.wait();
-
-            console.log("receipt: ", receipt);
-
-            if (receipt.status) {
-                return console.log("vote successfull!");
-            }
-
-            console.log("vote failed!");
-        } catch (error) {
-            console.log(error);
-            let errorText;
-            if (error.reason === "Has no right to vote") {
-                errorText = "You have not right to vote";
-            } else if (error.reason === "Already voted.") {
-                errorText = "You have already voted";
-            } else {
-                errorText = "An unknown error occured";
-            }
-
-            console.error("error: ", errorText);
-        }
-    };
 
     return (
         <Container>
@@ -58,24 +24,7 @@ function App() {
                 <Box mb="4">
                     <DelegateVote />
                 </Box>
-
-                <Flex wrap={"wrap"} gap={"6"}>
-                    {loading ? (
-                        <Text>Loading...</Text>
-                    ) : proposals.length !== 0 ? (
-                        proposals.map((item, index) => (
-                            <Proposal
-                                key={index}
-                                name={item.name}
-                                handleVote={handleVote}
-                                id={index}
-                                voteCount={Number(item.voteCount)}
-                            />
-                        ))
-                    ) : (
-                        <Text>Could not get proposals!!</Text>
-                    )}
-                </Flex>
+            <HandleVoter proposals={proposals} loading={loading} />               
             </main>
         </Container>
     );
---
+++ src/Components/HandleVoter.jsx
@@ -0,0 +1,75 @@
+import { useWeb3ModalAccount, useWeb3ModalProvider } from "@web3modal/ethers/react";
+import { isSupportedChain } from "../Utils";
+import { getProposalsContract } from "../Constants/contracts";
+import { getProvider } from "../Constants/providers";
+import Proposal from "./Proposal";
+import { Flex, Text } from "@radix-ui/themes";
+
+
+const HandleVoter = ({proposals, loading}) => {
+    const { chainId } = useWeb3ModalAccount();
+    const { walletProvider } = useWeb3ModalProvider();
+
+
+    const handleVote = async (id) => {
+        if (!isSupportedChain(chainId)) return console.error("Wrong network");
+        const readWriteProvider = getProvider(walletProvider);
+        const signer = await readWriteProvider.getSigner();
+
+        const contract = getProposalsContract(signer);
+
+        try {
+            const transaction = await contract.vote(id);
+            console.log("transaction: ", transaction);
+            const receipt = await transaction.wait();
+
+            console.log("receipt: ", receipt);
+
+            if (receipt.status) {
+                return console.log("vote successfull!");
+            }
+
+            console.log("vote failed!");
+        } catch (error) {
+            console.log(error);
+            let errorText;
+            if (error.reason === "Has no right to vote") {
+                errorText = "You have not right to vote";
+            } else if (error.reason === "Already voted.") {
+                errorText = "You have already voted";
+            } else {
+                errorText = "An unknown error occured";
+            }
+
+            console.error("error: ", errorText);
+        }
+
+    };
+
+    return (
+
+        <div>
+             <Flex wrap={"wrap"} gap={"6"}>
+            {loading ? (
+                <Text>Loading...</Text>
+            ) : proposals.length !== 0 ? (
+                proposals.map((item, index) => (
+                    <Proposal
+                        key={index}
+                        name={item.name}
+                        handleVote={handleVote}
+                        id={index}
+                        voteCount={Number(item.voteCount)}
+                    />
+                ))
+            ) : (
+                <Text>Could not get proposals!!</Text>
+            )}
+        </Flex>
+        </div>
+           
+         );
+    }
+
+
+export default HandleVoter
